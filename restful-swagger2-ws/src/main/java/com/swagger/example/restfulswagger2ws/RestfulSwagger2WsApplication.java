8f095f29f78137cd601c677ca19005187e6e36e8
Nayan Kumar H N <nayan.savi@gmail.com>
2019-05-06 20:22:55 -0700

    swagger2 example

---
+++ restful-swagger2-ws/src/main/java/com/swagger/example/restfulswagger2ws/RestfulSwagger2WsApplication.java
@@ -0,0 +1,13 @@
+package com.swagger.example.restfulswagger2ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSwagger2WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSwagger2WsApplication.class, args);
+	}
+
+}
